44f96e8647031ea94881585884d67397e1a1b54f
Humayun Imtiaz <humayunimtiaz81@gmail.com>
2026-07-16 11:57:23 -0700

    fix: add node globals to eslint config to resolve require/process errors

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -3,11 +3,23 @@ const tsPlugin = require("@typescript-eslint/eslint-plugin");
 const tsParser = require("@typescript-eslint/parser");
 
 module.exports = [
+    {
+        ignores: ["dist/**", "node_modules/**"],
+    },
     js.configs.recommended,
     {
         files: ["src/**/*.ts"],
         languageOptions: {
             parser: tsParser,
+            globals: {
+                process: "readonly",
+                console: "readonly",
+                module: "readonly",
+                require: "readonly",
+                __dirname: "readonly",
+                Buffer: "readonly",
+                global: "readonly",
+            },
         },
         plugins: {
             "@typescript-eslint": tsPlugin,
@@ -15,6 +27,15 @@ module.exports = [
         rules: {
             "no-unused-vars": "off",
             "@typescript-eslint/no-unused-vars": "warn",
+        },
+    },
+    {
+        files: ["eslint.config.js"],
+        languageOptions: {
+            globals: {
+                require: "readonly",
+                module: "readonly",
+            },
         },
     },
 ];
